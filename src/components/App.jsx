7e27de42372dbadf257ee41b7c96afd9fa05750c
Serhii Yevchishen <magodza06@gmail.com>
2023-06-29 23:12:46 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { GlobalStyle } from './GlobalStyle';
+// import initialContacts from './contacts.json';
+
+
+// const notifyOptions = {
+//   position: 'bottom-left',
+//   autoClose: 5000,
+//   hideProgressBar: false,
+//   closeOnClick: true,
+//   pauseOnHover: true,
+//   draggable: true,
+//   progress: undefined,
+//   theme: 'colored',
+// };
+
+// export default class App extends Component {
+//   state = {
+//     contacts: initialContacts,
+//     // filter: '',
+//   };
+
+
+  // componentDidMount() {
+  //   const contactsFromLS = localStorage.getItem('contacts');
+  //   //console.log(contactsFromLS);
+  //   const parsedContacts = JSON.parse(contactsFromLS);
+  //   if (!parsedContacts) return;
+  //   this.setState({ contacts: parsedContacts });
+  // }
